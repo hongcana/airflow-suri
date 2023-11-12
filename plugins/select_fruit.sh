6488138108ddcdfebfbf547737e4f95e87c3a086
hongcana <hongcana@kakao.com>
2023-11-12 16:36:41 +0900

    upload shell_example in plugins dir

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT = $1
+if [ $FRUIT == APPLE ]; then
+    echo "Apple!"
+if [ $FRUIT == ORANGE ]; then
+    echo "오링~지!"
+if [ $FRUIT == GRAPE ]; then
+    echo "Grape!"
+else
+    echo "Not Found, Try other command."
+fi
